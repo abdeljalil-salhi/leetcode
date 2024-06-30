67cfb9b15936540ddbf16eafacbe9108287d7d86
Abdel <abdelisloading@gmail.com>
2024-06-30 15:30:24 +0100

    684. Redundant Connection

---
+++ II. MEDIUM/684. Redundant Connection/solution.py
@@ -0,0 +1,38 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = {}
+        self.rank = {}
+        for i in range(n + 1):
+            self.parent[i] = i
+            self.rank[i] = 1
+
+    def find(self, n: int) -> int:
+        p = self.parent[n]
+        while p != self.parent[p]:
+            self.parent[p] = self.parent[self.parent[p]]
+            p = self.parent[p]
+        return p
+
+    def union(self, n1: int, n2: int) -> bool:
+        p1, p2 = self.find(n1), self.find(n2)
+
+        if p1 == p2:
+            return False
+
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += self.rank[p1]
+
+        return True
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        UF = UnionFind(len(edges))
+
+        for edge in edges:
+            if not UF.union(edge[0], edge[1]):
+                return edge
