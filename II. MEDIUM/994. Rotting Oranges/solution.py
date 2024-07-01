672cf4f7d4f5fbbbc04a6a1858d96ea36bd5d881
Abdel <abdelisloading@gmail.com>
2024-07-01 18:47:52 +0100

    994. Rotting Oranges

--- II. MEDIUM/994. Rotting Oranges/solution.py
+++ II. MEDIUM/994. Rotting Oranges/solution.py
@@ -1,30 +1,35 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        fresh = minutes = rotten = 0
         deq = deque()
-        minutes = fresh = size = 0
+        visited = set()
 
-        R, C = len(grid), len(grid[0])
-        for i in range(R):
-            for j in range(C):
-                if grid[i][j] == 1:
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == 1:
                     fresh += 1
-                elif grid[i][j] == 2:
-                    deq.append((i, j))
-                    size += 1
+                elif grid[row][col] == 2:
+                    deq.append((row, col))
+                    rotten += 1
 
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         while deq and fresh:
-            for _ in range(size):
-                i, j = deq.popleft()
-                size -= 1
+            for _ in range(rotten):
+                r, c = deq.popleft()
+                rotten -= 1
 
-                for di, dj in directions:
-                    row, col = i + di, j + dj
-                    if 0 <= row < R and 0 <= col < C and grid[row][col] == 1:
-                        grid[row][col] = 2
+                for drow, dcol in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    nr, nc = r + drow, c + dcol
+                    if (
+                        0 <= nr < ROWS
+                        and 0 <= nc < COLS
+                        and (nr, nc) not in visited
+                        and grid[nr][nc] == 1
+                    ):
+                        visited.add((nr, nc))
+                        deq.append((nr, nc))
+                        rotten += 1
                         fresh -= 1
-                        deq.append((row, col))
-                        size += 1
 
             minutes += 1
 
