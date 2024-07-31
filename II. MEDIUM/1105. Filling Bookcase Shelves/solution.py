b64af80d125f7c0a3cb88f99fac1541ec6c37498
Abdel <abdelisloading@gmail.com>
2024-07-31 05:40:28 +0100

    1105. Filling Bookcase Shelves

---
+++ II. MEDIUM/1105. Filling Bookcase Shelves/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
+        n = len(books)
+        dp = [0] * (n + 1)
+
+        for i, (width, height) in enumerate(books, 1):
+            dp[i] = dp[i - 1] + height
+
+            for j in range(i - 1, 0, -1):
+                width += books[j - 1][0]
+                if width > shelfWidth:
+                    break
+                height = max(height, books[j - 1][1])
+                dp[i] = min(dp[i], dp[j - 1] + height)
+
+        return dp[n]
--- README.md
+++ README.md
@@ -19,7 +19,7 @@
   <tr>
     <td width="30px"></td>
     <td align="center">
-      <a href="/II. MEDIUM/1653. Minimum Deletions to Make String Balanced/">
+      <a href="/II. MEDIUM/1105. Filling Bookcase Shelves/">
         <img src="https://github.com/abdeljalil-salhi/leetcode/assets/65598953/fabf3ea9-4ec8-4297-aebf-50605e1fe6da" width="100px;" alt="LeetCode Streak"/><br>
         <p>
           <b>Visit >></b>
